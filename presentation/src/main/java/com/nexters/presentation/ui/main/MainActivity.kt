5655fead5598df7568b00ee314f616ed8a7094fb
박진성 <plashdof@gmail.com>
2024-07-19 05:31:33 +0900

    [chore] #1 Activity Flow Setting

---
+++ presentation/src/main/java/com/nexters/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nexters.presentation.ui.main
+
+import android.os.Bundle
+import com.nexters.presentation.base.BaseActivity
+import com.nexters.presentation.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
