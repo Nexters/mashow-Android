426b4a4d5265edb04bf29d10818ca64075cd5265
박진성 <plashdof@gmail.com>
2024-08-08 00:14:25 +0900

    [feat] #9 기록 네비게이션 셋팅

---
+++ presentation/src/main/java/com/masshow/presentation/ui/main/record/food/FoodRecordFragment.kt
@@ -0,0 +1,16 @@
+package com.masshow.presentation.ui.main.record.food
+
+import android.os.Bundle
+import android.view.View
+import com.masshow.presentation.R
+import com.masshow.presentation.base.BaseFragment
+import com.masshow.presentation.databinding.FragmentFoodRecordBinding
+
+class FoodRecordFragment: BaseFragment<FragmentFoodRecordBinding>(R.layout.fragment_food_record) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
