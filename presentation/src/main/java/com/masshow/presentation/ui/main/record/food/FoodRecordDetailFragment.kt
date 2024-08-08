95e67ea1bae6e44d72c12fdc3a4288538f958e3d
박진성 <plashdof@gmail.com>
2024-08-08 23:59:32 +0900

    [feat] #9 음식 기록 상세 Ui 일부 구현

---
+++ presentation/src/main/java/com/masshow/presentation/ui/main/record/food/FoodRecordDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.masshow.presentation.ui.main.record.food
+
+import android.os.Bundle
+import android.view.View
+import com.masshow.presentation.R
+import com.masshow.presentation.base.BaseFragment
+import com.masshow.presentation.databinding.FragmentFoodRecordDetailBinding
+
+class FoodRecordDetailFragment : BaseFragment<FragmentFoodRecordDetailBinding>(R.layout.fragment_food_record_detail){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
