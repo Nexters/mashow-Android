4d23a7e8ca31e8134d737d4c2c204886bb35d628
박진성 <plashdof@gmail.com>
2024-08-08 23:59:51 +0900

    [feat] #9 음식 기록 메인 -> 음식 기록 상세 화면이동 처리

--- presentation/src/main/java/com/masshow/presentation/ui/main/record/food/FoodRecordFragment.kt
+++ presentation/src/main/java/com/masshow/presentation/ui/main/record/food/FoodRecordFragment.kt
@@ -2,14 +2,40 @@ package com.masshow.presentation.ui.main.record.food
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.masshow.presentation.R
 import com.masshow.presentation.base.BaseFragment
 import com.masshow.presentation.databinding.FragmentFoodRecordBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class FoodRecordFragment: BaseFragment<FragmentFoodRecordBinding>(R.layout.fragment_food_record) {
+@AndroidEntryPoint
+class FoodRecordFragment : BaseFragment<FragmentFoodRecordBinding>(R.layout.fragment_food_record) {
+
+    private val viewModel: FoodRecordViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
+        initEventObserve()
+    }
+
+    private fun initEventObserve() {
+        repeatOnStarted {
+            viewModel.event.collect {
+                when (it) {
+                    is FoodRecordEvent.NavigateToFoodRecordDetail -> findNavController().toFoodRecordDetail()
+                }
+            }
+        }
+    }
+
+    private fun NavController.toFoodRecordDetail() {
+        val action =
+            FoodRecordFragmentDirections.actionFoodRecordFragmentToFoodRecordDetailFragment()
+        navigate(action)
     }
 
 
