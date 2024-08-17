25ba71ffc0ba27093d9479cf743796e463e4f98b
박진성 <plashdof@gmail.com>
2024-08-17 14:43:33 +0900

    [feat] #12 HomeFragment 생성

---
+++ presentation/src/main/java/com/masshow/presentation/ui/main/home/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.masshow.presentation.ui.main.home
+
+import com.masshow.presentation.R
+import com.masshow.presentation.base.BaseFragment
+import com.masshow.presentation.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+}
