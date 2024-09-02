eb1e16eab907741548adbc89cad38f5e81e4bb2c
박진성 <plashdof@gmail.com>
2024-09-02 22:56:29 +0900

    [feat] #15 feat : 평가하기 스와이프 로직 구현

--- presentation/src/main/java/com/masshow/presentation/ui/main/record/estimate/EstimateFragment.kt
+++ presentation/src/main/java/com/masshow/presentation/ui/main/record/estimate/EstimateFragment.kt
@@ -1,18 +1,80 @@
 package com.masshow.presentation.ui.main.record.estimate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup.LayoutParams
+import androidx.constraintlayout.widget.ConstraintSet.Motion
 import com.masshow.presentation.R
 import com.masshow.presentation.base.BaseFragment
 import com.masshow.presentation.databinding.FragmentEstimateBinding
+import com.masshow.presentation.util.Constants.TAG
+import kotlin.math.max
+import kotlin.math.min
 
 class EstimateFragment: BaseFragment<FragmentEstimateBinding>(R.layout.fragment_estimate) {
 
+    private var initialY = 0f
+    private var initialHeight = 0
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//
+//        val initialHeightDp = 200
+//        val displayMetrics = resources.displayMetrics
+//        initialHeight = (initialHeightDp * displayMetrics.density).toInt()
+//
+//        binding.viewSwipable.layoutParams = binding.viewSwipable.layoutParams.apply{
+//            height = initialHeight
+//        }
+
+        binding.viewSwipable.setOnTouchListener { v, event ->
+            when(event.action){
+                MotionEvent.ACTION_DOWN -> {
+                    initialY = event.rawY
+                    initialHeight = binding.swipeView.height
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    Log.d(TAG,event.rawY.toString())
+                    binding.swipeView.layoutParams = (binding.swipeView.layoutParams as LayoutParams).apply {
+                        height = (v.height - event.rawY).toInt() + 400
+                    }
+                    binding.swipeView.requestLayout()
+                    true
+                }
+                MotionEvent.ACTION_UP -> true
+                else -> false
+            }
+        }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
