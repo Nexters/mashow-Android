072628521f94c68ccb6d142eded769fabd406495
박진성 <plashdof@gmail.com>
2024-09-03 19:33:01 +0900

    [feat] #15 feat : 평가하기 스와이프 로직에 따른 캐릭터 visibility 설정 수정

--- presentation/src/main/java/com/masshow/presentation/ui/main/record/estimate/EstimateFragment.kt
+++ presentation/src/main/java/com/masshow/presentation/ui/main/record/estimate/EstimateFragment.kt
@@ -14,7 +14,7 @@ import com.masshow.presentation.util.Constants.TAG
 import kotlin.math.max
 import kotlin.math.min
 
-class EstimateFragment: BaseFragment<FragmentEstimateBinding>(R.layout.fragment_estimate) {
+class EstimateFragment : BaseFragment<FragmentEstimateBinding>(R.layout.fragment_estimate) {
 
     private var initialY = 0f
     private var initialHeight = 0
@@ -32,50 +32,55 @@ class EstimateFragment: BaseFragment<FragmentEstimateBinding>(R.layout.fragment_
 //        }
 
         binding.viewSwipable.setOnTouchListener { v, event ->
-            when(event.action){
+            when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     initialY = event.rawY
                     initialHeight = binding.swipeView.height
                     true
                 }
+
                 MotionEvent.ACTION_MOVE -> {
-                    Log.d(TAG,event.rawY.toString())
-                    binding.swipeView.layoutParams = (binding.swipeView.layoutParams as LayoutParams).apply {
-                        height = (v.height - event.rawY).toInt() + 400
+
+                    binding.swipeView.layoutParams =
+                        (binding.swipeView.layoutParams as LayoutParams).apply {
+                            height = (v.height - event.rawY).toInt() + 400
+                        }
+
+                    val point = (v.height - event.rawY).toInt() + 400
+
+                    if (point in binding.root.height - binding.tvUmm.y.toInt()..binding.root.height - binding.tvNotBad.y.toInt()) {
+                        binding.ivUmm.visibility = View.VISIBLE
+                        binding.ivNotBad.visibility = View.INVISIBLE
+                    } else if (point in binding.root.height - binding.tvNotBad.y.toInt()..binding.root.height - binding.tvGood.y.toInt()) {
+                        binding.ivUmm.visibility = View.INVISIBLE
+                        binding.ivNotBad.visibility = View.VISIBLE
+                        binding.ivGood.visibility = View.INVISIBLE
+                    } else if (point in binding.root.height - binding.tvGood.y.toInt()..binding.root.height - binding.tvVeryGood.y.toInt()) {
+                        binding.ivNotBad.visibility = View.INVISIBLE
+                        binding.ivGood.visibility = View.VISIBLE
+                        binding.ivVeryGood.visibility = View.INVISIBLE
+                    } else if (point in binding.root.height - binding.tvVeryGood.y.toInt()..binding.root.height - binding.tvAwesome.y.toInt()) {
+                        binding.ivGood.visibility = View.INVISIBLE
+                        binding.ivVeryGood.visibility = View.VISIBLE
+                        binding.ivAwsome.visibility = View.INVISIBLE
+                    } else if (point >= binding.root.height - binding.tvAwesome.y.toInt()) {
+                        binding.ivVeryGood.visibility = View.INVISIBLE
+                        binding.ivAwsome.visibility = View.VISIBLE
+                    } else if (point < binding.root.height - binding.tvUmm.y.toInt()) {
+                        binding.ivUmm.visibility = View.INVISIBLE
                     }
+
+
                     binding.swipeView.requestLayout()
                     true
                 }
+
                 MotionEvent.ACTION_UP -> true
                 else -> false
             }
         }
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
