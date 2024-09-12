f442cbb09fc1b99ad3f1462d117793ab4fa87696
박진성 <plashdof@gmail.com>
2024-09-13 00:38:53 +0900

    [feat] #9 feat : 안주 추가 구현

--- presentation/src/main/java/com/masshow/presentation/ui/main/record/food/FoodRecordFragment.kt
+++ presentation/src/main/java/com/masshow/presentation/ui/main/record/food/FoodRecordFragment.kt
@@ -1,7 +1,12 @@
 package com.masshow.presentation.ui.main.record.food
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -21,7 +26,9 @@ class FoodRecordFragment : BaseFragment<FragmentFoodRecordBinding>(R.layout.frag
 
         binding.vm = viewModel
         initEventObserve()
-        binding.tvFoods.text = RecordFormData.foods.joinToString()
+        RecordFormData.foods.forEach {
+            addFood(it)
+        }
     }
 
     private fun initEventObserve() {
@@ -38,6 +45,16 @@ class FoodRecordFragment : BaseFragment<FragmentFoodRecordBinding>(R.layout.frag
         val action =
             FoodRecordFragmentDirections.actionFoodRecordFragmentToFoodRecordDetailFragment()
         navigate(action)
+    }
+
+    private fun addFood(text: String) {
+        val newFood = LayoutInflater.from(requireContext())
+            .inflate(R.layout.item_food, binding.layoutFood, false)
+        val food = newFood.findViewById<TextView>(R.id.tv_text)
+
+        food.text = text
+
+        binding.layoutFood.addView(newFood)
     }
 
 
