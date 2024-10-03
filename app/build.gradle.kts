325b569f8f58b724d03f81656e31579b32177e6a
박진성 <plashdof@gmail.com>
2024-10-03 21:40:49 +0900

    [feat] #20 version : 프로덕션 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.masshow.alchol"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_DEV_URL", getProperty("baseDevUrl"))
