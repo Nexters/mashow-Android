9285b425ab03f12ef18791f7777d3131f9e80e31
박진성 <plashdof@gmail.com>
2024-10-08 22:12:50 +0900

    [feat] #23 version : 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.masshow.alchol"
         minSdk = 28
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.1.3"
+        versionCode = 6
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_DEV_URL", getProperty("baseDevUrl"))
